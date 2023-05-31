f676883a4fec9cbcee6979842ae1295f24906da3
Philippe <philippe.idalgoprestes@gmail.com>
2023-05-31 14:37:29 -0300

    Interatividade das páginas de cadastro e login finalizadas

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,14 +1,34 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
+import axios from 'axios'
+import { useState, createContext, useContext} from 'react'
 
 export default function LoginPage(){
+    let [password, setPassword] = useState(null)
+    let [email, setEmail] = useState(null)
+    let [dis, setDis] = useState(false)
+    let disAbled = createContext(dis);
+    let navigate = useNavigate();
+    function fazer_cadastro(e){
+        setDis(!dis);
+        e.preventDefault();
+        const dados = {email: email, password: password};
+        console.log(dados);
+        const requisicao = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", dados);
+        requisicao.then(resposta => {
+            console.log(resposta.data);
+            navigate('/hoje');});
+        requisicao.catch(resposta => {
+            alert(resposta.response.data.message);
+            setDis(dis);})
+    }
     return(
         <>
             <Logo><img src="https://imagepng.org/wp-content/uploads/2017/10/quadrado-preto-1.png"/></Logo>
             <Forms>
-                <input placeholder='email'data-test="email-input"/>
-                <input placeholder='senha' data-test="password-input"/>
-                <button data-test="login-btn">Entrar</button>
+                <input disabled={useContext(disAbled)} placeholder='email'data-test="email-input" onChange={(e) => setEmail(e.target.value)}/>
+                <input disabled={useContext(disAbled)} type='password' placeholder='senha' data-test="password-input" onChange={(e) => setPassword(e.target.value)}/>
+                <button disabled={useContext(disAbled)} onClick={(e) => fazer_cadastro(e)} data-test="login-btn">Entrar</button>
             </Forms>
             <ToLink><Link to="/cadastro" data-test="signup-link">Não tem uma conta? Cadastre-se!</Link></ToLink>
         </>
@@ -19,6 +39,7 @@ const Logo = styled.div`
     width: 100%;
     padding: 30px 0px;
     text-align: center;
+    height: auto;
     img{
         width: 50%;
     }
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,16 +1,38 @@
 import styled from "styled-components"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { useState, createContext, useContext } from 'react'
 
 export default function SignupPage(){
+    let [email, setEmail] = useState(null)
+    let [password, setPassword] = useState(null)
+    let [name, setName] = useState(null)
+    let [photo, setPhoto] = useState(null)
+    let [dis, setDis] = useState(false);
+    let disAbled = createContext(dis);
+    let navigate = useNavigate();
+    function fazer_cadastro(e){
+        setDis(!dis);
+        e.preventDefault();
+        const dados = {email: email, name: name, image: photo, password: password};
+        console.log(dados);
+        const requisicao = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up", dados);
+        requisicao.then(resposta => {
+            console.log(resposta.data);
+            navigate('/');});
+        requisicao.catch(resposta => {
+            alert(resposta.response.data.message);
+            setDis(dis);})
+    }
     return(
         <>
             <Logo><img src="https://imagepng.org/wp-content/uploads/2017/10/quadrado-preto-1.png"/></Logo>
             <Forms>
-                <input placeholder='email' data-test="email-input"/>
-                <input placeholder='senha' data-test="password-input"/>
-                <input placeholder='nome' data-test="user-name-input"/>
-                <input placeholder='foto' data-test="user-image-input"/>
-                <button data-test="signup-btn">Cadastrar</button>
+                <input disabled={useContext(disAbled)} placeholder='email' data-test="email-input" onChange={(e) => setEmail(e.target.value)}/>
+                <input disabled={useContext(disAbled)} type="password" placeholder='senha' data-test="password-input"  onChange={(e) => setPassword(e.target.value)}/>
+                <input disabled={useContext(disAbled)} placeholder='nome' data-test="user-name-input" onChange={(e) => setName(e.target.value)}/>
+                <input disabled={useContext(disAbled)} placeholder='foto' data-test="user-image-input" onChange={(e) => setPhoto(e.target.value)}/>
+                <button disabled={useContext(disAbled)} data-test="signup-btn" onClick={(e) => fazer_cadastro(e)}>Cadastrar</button>
             </Forms>
             <ToLink><Link to="/" data-test="login-link">Já tem uma conta? Faça login!</Link></ToLink>
         </>
@@ -21,6 +43,7 @@ const Logo = styled.div`
     width: 100%;
     padding: 30px 0px;
     text-align: center;
+    height: auto;
     img{
         width: 50%;
     }
